211b549a912481d4bfd643a5790547654b089015
rajib ganguly <v-rganguly@microsoft.com>
2023-05-18 19:11:31 +0530

    demo test

--- services/data.js
+++ services/data.js
@@ -3,26 +3,26 @@ const data = {
       {
         id: 1,
         task: 'task1',
-        assignee: 'assignee1000',
-        status: 'completed'
+        name: 'assignee1000',
+        status: 'Relevant'
       },
       {
         id: 2,
         task: 'task2',
-        assignee: 'assignee1001',
-        status: 'completed'
+        name: 'assignee1001',
+        status: ''
       },
       {
         id: 3,
         task: 'task3',
-        assignee: 'assignee1002',
-        status: 'completed'
+        name: 'assignee1002',
+        status: 'Not Relevant'
       },
       {
         id: 4,
         task: 'task4',
-        assignee: 'assignee1000',
-        status: 'completed'
+        name: 'assignee1000',
+        status: 'Relevant'
       },
       
     ]
--- todo-get/index.js
+++ todo-get/index.js
@@ -2,7 +2,7 @@ const todoService = require('../services/todo');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    context.log(todoService.getTodos(context))
+    context.log(todoService.getTodos(context), req)
 
     context.res = {
         status: 200,
