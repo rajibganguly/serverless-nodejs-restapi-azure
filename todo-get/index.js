1eeed4a0f7eec4957297fd707c4504d336cf760b
rajib ganguly <v-rganguly@microsoft.com>
2023-05-18 19:01:01 +0530

    added

--- todo-get/index.js
+++ todo-get/index.js
@@ -2,6 +2,7 @@ const todoService = require('../services/todo');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    context.log(todoService.getTodos(context))
 
     context.res = {
         status: 200,
